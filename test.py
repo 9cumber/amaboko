b89bcd8a5362c7ab9dcc4eefcbb566ad9de18146
michael <micfall.romtin@gmail.com>
2016-06-02 03:31:55 +0900

    simple test for module

--- test.py
+++ test.py
@@ -2,11 +2,16 @@
 
 import api
 
-isbn_list = ["978-4-00-310101-8", "978-4-10-109205-8", "978-4-04-315101-8"]
+isbn_list = ["978-4048916592", "978-1433551666", "978-4-04-315101-8"]
 
 ama_book = api.AmazonBook()
 
 for isbn in isbn_list:
-    print "This is test for " + isbn
+    print "-- This is test for " + isbn
     print "| " + ama_book.isbn_format(isbn)
     print "| " + str(ama_book.isbn_validation(isbn))
+    books = ama_book.lookup(isbn)
+    book = books[0] if isinstance(books, list) else books
+    if book is not None:
+        print "| " + book.title
+        print "| " + book.price_and_currency[0]
